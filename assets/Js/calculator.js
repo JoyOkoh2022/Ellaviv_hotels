137cbd404b8a93077a95cb3170c27edfcbdc6fe2
JoyOkoh2022 <111923181+JoyOkoh2022@users.noreply.github.com>
2022-09-30 22:46:51 +0100

    Add files via upload
    Ellaviv hotels project

---
+++ assets/Js/calculator.js
@@ -0,0 +1,54 @@
+console.log("calculator is running")
+// this program adds two numbers
+//and prints the result
+
+
+function add_numbers(){
+	
+let x =document.getElementById('frst').value
+let y =document.getElementById('scnd').value
+let z=parseInt(x)+parseInt(y)
+console.log(z)
+let msg="The sum of  "+x+" and "+y+" is "+z
+let dsp =document.getElementById('dsply').innerHTML=msg
+}
+
+
+
+function sub_numbers(){
+	
+let x =document.getElementById('frst').value
+let y =document.getElementById('scnd').value
+let z=parseInt(x)-parseInt(y)
+console.log(z)
+let msg="The difference of "+ x+" and "+y+" is "+ z
+let dsp =document.getElementById('dsply').innerHTML=msg
+	
+}
+
+
+
+function mul_numbers(){
+	
+let x =document.getElementById('frst').value
+let y =document.getElementById('scnd').value
+let z=parseInt(x)*parseInt(y)
+console.log(z)
+let msg="The product of "+ x+" and "+y+" is "+ z
+let dsp =document.getElementById('dsply').innerHTML=msg
+	
+}
+
+
+
+
+function div_numbers(){
+	
+let x =document.getElementById('frst').value
+let y =document.getElementById('scnd').value
+let z=parseInt(x)/parseInt(y)
+console.log(z)
+let msg="The quotient of "+ x+" and "+y+" is "+ z
+let dsp =document.getElementById('dsply').innerHTML=msg
+	
+}
---
+++ assets/Js/menu.js
@@ -0,0 +1,18 @@
+let x = document.getElementById("k_items")
+
+let t1='<div class="k_item">'
+let t2='<div class="k_item_title">Intercontinental Dishes </div>'
+let t3='<img src="assets/images/intercontinentaldishes1.jpg">'
+let t4='<button > Order this item</button>'
+let t5='</div>'
+
+let t6='<div class="k_item">'
+let t7='<div class="k_item_title">Intercontinental Dishes </div>'
+let t8='<img src="assets/images/intercontinentaldishes2.jpg">'
+let t9='<button > Order this item</button>'
+let t10='</div>'
+
+
+let data=t1+t2+t3+t4+t5+t6+t7+t8+t9+t10
+
+x.innerHTML=data
